da1668708e69f21fec5ecfa7e4c29e93be4a8f52
gitYeawon <kim8529635@gmail.com>
2021-07-07 23:11:44 +0200

    difference between scanf() and gets()

--- newC/main.c
+++ newC/main.c
@@ -7,25 +7,14 @@
 //
 
 #include <stdio.h>
-
+#pragma warning(disable:4996)
 void main() {
-    int a;
-    long long int b;
-    float c;
-    char ch;
-    char s[20];
-    printf("Enter a integer and long long integer : ");
-    scanf("%d %lld", &a, &b);
-    printf("a = %d, b = %lld \n", a, b);
-    printf("Enter real numbers : ");
-    scanf("%f", &c);
-    printf("c = %10.4f, c(exponet) = %e \n", c,c);
-    printf("Enter a letter :");
-    scanf(" %c", &ch);
-    //make a space front of the %c in order to ignore the enter key from the last input of scanf().
-    printf("ch = %c \n", ch);
-    printf("Enter strings : ");
+    char s[50];
+    printf("Enter strings? ");
+    gets(s);
+    printf("strings using gets() = %s \n", s); // save entire strings
+    printf("Enrer strings? ");
     scanf("%s", s);
-    printf("s[] = %s \n", s);
+    printf("strings using scanf() = %s \n", s); //does not save the rest of the strings after space
 }
 
