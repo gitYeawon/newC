d34c0295a88ac7e014cd8763242ec55d0ca41d8c
gitYeawon <kim8529635@gmail.com>
2021-07-09 23:41:59 +0200

    unary operation with comments

--- newC/operator.c
+++ newC/operator.c
@@ -7,13 +7,13 @@
 //
 
 #include "operator.h"
-void main_2() {
-    int a, b, c;
+void main_2() {             //x = i++ => x = 5, i =6
+    int a, b, c;            //x = ++i => x = 6, i =6
     a = b= c = 0;
     a = ++b + ++c;
-    printf("a = ++b + ++c 실행 후 a = %d, b = %d, c =%d \n",a,b,c);
+    printf("a = ++b + ++c 실행 후 a = %d, b = %d, c =%d \n",a,b,c); //a=2, b=1, c=1
     a = b++ + c++;
-    printf("a = b++ + c++ 실행 후 a = %d, b = %d, c =%d \n",a,b,c);
+    printf("a = b++ + c++ 실행 후 a = %d, b = %d, c =%d \n",a,b,c); //a=2, b=2, c=2
     a = b-- + --c;
-    printf("a = b-- + --c 실행 후 a = %d, b = %d, c =%d \n",a,b,c);
+    printf("a = b-- + --c 실행 후 a = %d, b = %d, c =%d \n",a,b,c); //a=3, b=1, c=1
 }
